ec056bae04c53bcb57779dff21dee99e1bc30190
mikeyrayvon <mikey@interglobal.vision>
2018-10-29 10:26:48 -0600

    add metabox fields

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -39,5 +39,87 @@ function igv_cmb_metaboxes() {
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
    */
 
+  $home_page = get_page_by_path('home');
+
+  if(!empty($home_page)) {
+
+    $home_metabox = new_cmb2_box( array(
+  		'id'            => $prefix . 'home_metabox',
+  		'title'         => esc_html__( 'Fields', 'cmb2' ),
+  		'object_types'  => array( 'page' ), // Post type
+      'show_on'      => array(
+        'key' => 'id',
+        'value' => array($home_page->ID)
+      ),
+  	) );
+
+    $home_metabox->add_field( array(
+      'name'       => esc_html__( 'Prize', 'cmb2' ),
+      'id'         => $prefix . 'home_prize',
+      'type'       => 'wysiwyg',
+      'options' => array(
+        'textarea_rows' => get_option('default_post_edit_rows', 16), // rows="..."
+      ),
+    ) );
+
+    $judges_group = $home_metabox->add_field( array(
+      'id'          => $prefix . 'home_judges',
+      'type'        => 'group',
+      'description' => esc_html__( 'Judges', 'cmb2' ),
+      'options'     => array(
+        'group_title'   => esc_html__( 'Judge {#}', 'cmb2' ), // {#} gets replaced by row number
+        'add_button'    => esc_html__( 'Add Another Judge', 'cmb2' ),
+        'remove_button' => esc_html__( 'Remove Judge', 'cmb2' ),
+        'sortable'      => true, // beta
+        // 'closed'     => true, // true to have the groups closed by default
+      ),
+    ) );
+
+    $home_metabox->add_group_field( $judges_group, array(
+      'name'       => esc_html__( 'Name', 'cmb2' ),
+      'id'         => 'name',
+      'type'       => 'text',
+      'description' => esc_html__( '', 'cmb2' ),
+    ) );
+
+    $home_metabox->add_group_field( $judges_group, array(
+      'name'       => esc_html__( 'Bio', 'cmb2' ),
+      'id'         => 'bio',
+      'type'       => 'wysiwyg',
+      'description' => esc_html__( '', 'cmb2' ),
+      'options' => array(
+        'media_buttons' => false, // show insert/upload button(s)
+        'textarea_rows' => get_option('default_post_edit_rows', 5), // rows="..."
+      ),
+    ) );
+
+    $home_metabox->add_field( array(
+      'name'       => esc_html__( 'Apply', 'cmb2' ),
+      'id'         => $prefix . 'home_apply',
+      'type'       => 'wysiwyg',
+      'options' => array(
+        'textarea_rows' => get_option('default_post_edit_rows', 16), // rows="..."
+      ),
+    ) );
+
+    $home_metabox->add_field( array(
+      'name'       => esc_html__( 'Dates', 'cmb2' ),
+      'id'         => $prefix . 'home_dates',
+      'type'       => 'wysiwyg',
+      'options' => array(
+        'textarea_rows' => get_option('default_post_edit_rows', 8), // rows="..."
+      ),
+    ) );
+
+    $home_metabox->add_field( array(
+      'name'       => esc_html__( 'Contact', 'cmb2' ),
+      'id'         => $prefix . 'home_contact',
+      'type'       => 'wysiwyg',
+      'options' => array(
+        'textarea_rows' => get_option('default_post_edit_rows', 8), // rows="..."
+      ),
+    ) );
+
+  }
 }
 ?>
